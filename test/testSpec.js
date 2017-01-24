6a1794f0f6baa5a92e2e9564d97979efd96ec121
Dan Novograd <dnovograd@gmail.com>
2017-01-23 19:39:59 -0500

    (dev) Add dev tools for build and coverage

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,4 +1,5 @@
-const {expect, assert} = require('chai');
+
+const { expect, assert } = require('chai');
 const generator = require('../index.js');
 const { fnRunner } = require('./test_helpers.js');
 const { generateNumber, generateString, generateBoolean, generateObject, generateArray } = generator;
